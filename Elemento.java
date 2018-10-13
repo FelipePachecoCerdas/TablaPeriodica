24fe629b5995f649f29d866ee2d6d84422e282a3
FelipePachecoCerdas <43649196+FelipePachecoCerdas@users.noreply.github.com>
2018-10-13 07:28:34 -0600

    Commit Inicial
    Se suben los archivos ya avanzados de interfaz grafica y las clases principales de TablaPeriodica y Elemento

---
+++ Elemento.java
@@ -0,0 +1,229 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quimica;
+
+/**
+ *
+ * @author felip
+ */
+public class Elemento {
+    String nombre;
+    int pesoAtomico;
+    String simbolo;
+    double masaAtomica;
+    String color;
+    String configuracionElectronica;
+    double electronegatividad;
+    int radioAtomico;
+    String radioIonico;
+    int radioVanDelWaals;
+    int energiaIonizacion;
+    int afinidadElectronica;
+    int estadosOxidacion[];
+    String estadoEstandar;
+    String tipo;
+    int puntoFusion;
+    int puntoEbullicion;
+    double densidad;
+    String nombreBloque;
+    int annoDescubrimiento;
+    
+public Elemento(String pNombre, int pPesoAtomico, String pSimbolo, double pMasaAtomica, String pColor, String pConfiguracionElectronica, double pElectronegatividad, int pRadioAtomico, String pRadioIonico, int pRadioVanDelWaals, int pEnergiaIonizacion, int pAfinidadElectronica, int pEstadosOxidacion[], String pEstadoEstandar, String pTipo, int pPuntoFusion, int pPuntoEbullicion, double pDensidad, String pNombreBloque, int pAnnoDescubrimiento) {
+        setNombre(pNombre);
+        setPesoAtomico(pPesoAtomico);
+        setSimbolo(pSimbolo);
+        setMasaAtomica(pMasaAtomica);
+        setColor(pColor);
+        setConfiguracionElectronica(pConfiguracionElectronica);
+        setElectronegatividad(pElectronegatividad);
+        setRadioAtomico(pRadioAtomico);
+        setRadioIonico(pRadioIonico);
+        setRadioVanDelWaals(pRadioVanDelWaals);
+        setEnergiaIonizacion(pEnergiaIonizacion);
+        setAfinidadElectronica(pAfinidadElectronica);
+        setEstadosOxidacion(pEstadosOxidacion);
+        setEstadoEstandar(pEstadoEstandar);
+        setTipo(pTipo);
+        setPuntoFusion(pPuntoFusion);
+        setPuntoEbullicion(pPuntoEbullicion);
+        setDensidad(pDensidad);
+        setNombreBloque(pNombreBloque);
+        setAnnoDescubrimiento(pAnnoDescubrimiento);
+    }
+
+    public Elemento() {
+        
+    }
+    
+    public String getEstadosOxidacionStr() {
+        String estados = "";
+        for (int i = 0; i < estadosOxidacion.length; i++) {
+            estados += (i == estadosOxidacion.length -1) ? estadosOxidacion[i]: estadosOxidacion[i] + ", ";
+        }
+        return estados;
+    }
+    public void setNombre(String pNombre) {
+        nombre = pNombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+        
+    public void setPesoAtomico(int pPesoAtomico) {
+        pesoAtomico = pPesoAtomico;
+    }
+    
+    public int getPesoAtomico() {
+        return pesoAtomico;
+    }
+        
+    public void setSimbolo(String pSimbolo) {
+        simbolo = pSimbolo;
+    }
+    
+    public String getSimbolo() {
+        return simbolo;
+    }
+        
+    public void setMasaAtomica(double pMasaAtomica) {
+        masaAtomica = pMasaAtomica;
+    }
+    
+    public double getMasaAtomica() {
+        return masaAtomica;
+    }
+        
+    public void setColor(String pColor) {
+        color = pColor;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    
+    public void setConfiguracionElectronica(String pConfiguracionElectronica) {
+        configuracionElectronica = pConfiguracionElectronica;
+    }
+    
+    public String getConfiguracionElectronica() {
+        return configuracionElectronica;
+    }
+
+    public void setElectronegatividad(double pElectronegatividad) {
+        electronegatividad = pElectronegatividad;
+    }
+    
+    public double getElectronegatividad() {
+        return electronegatividad;
+    }
+    
+    public void setRadioAtomico(int pRadioAtomico) {
+        radioAtomico = pRadioAtomico;
+    }
+    
+    public int getRadioAtomico() {
+        return radioAtomico;
+    }
+    
+    public void setRadioIonico(String pRadioIonico) {
+        radioIonico = pRadioIonico;
+    }
+    
+    public String getRadioIonico() {
+        return radioIonico;
+    }
+    
+    public void setRadioVanDelWaals(int pRadioVanDelWaals) {
+        radioVanDelWaals = pRadioVanDelWaals;
+    }
+    
+    public int getRadioVanDelWaals() {
+        return radioVanDelWaals;
+    }
+    
+    public void setEnergiaIonizacion (int pEnergiaIonizacion) {
+        energiaIonizacion = pEnergiaIonizacion;
+    }
+    
+    public int getEnergiaIonizacion() {
+        return energiaIonizacion;
+    }
+    
+    public void setAfinidadElectronica(int pAfinidadElectronica) {
+        afinidadElectronica = pAfinidadElectronica;
+    }
+    
+    public  int getAfinidadElectronica() {
+        return afinidadElectronica;
+    }
+    
+    public void setEstadosOxidacion(int[] pEstadosOxidacion) {
+        estadosOxidacion = pEstadosOxidacion;
+    }
+    
+    public int[] getEstadosOxidacion() {
+        return estadosOxidacion;
+    }
+
+    public void setEstadoEstandar(String pEstadoEstandar) {
+        estadoEstandar = pEstadoEstandar;
+    }
+    
+    public String getEstadoEstandar() {
+        return estadoEstandar;
+    }
+    
+    public void setTipo(String pTipo) {
+        tipo = pTipo;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
+    public void setPuntoFusion(int pPuntoFusion) {
+        puntoFusion = pPuntoFusion;
+    }
+    
+    public int getPuntoFusion() {
+        return puntoFusion;
+    }
+    
+    public void setPuntoEbullicion(int pPuntoEbullicion) {
+        puntoEbullicion = pPuntoEbullicion;
+    }
+    
+    public int getPuntoEbullicion() {
+        return puntoEbullicion;
+    }
+    
+    public void setDensidad(double pDensidad) {
+        densidad = pDensidad;
+    }
+    
+    public double getDensidad() {
+        return densidad;
+    }
+ 
+    public void setNombreBloque(String pNombreBloque) {
+        nombreBloque = pNombreBloque;
+    }
+    
+    public String getNombreBloque() {
+        return nombreBloque;
+    }
+    
+    public void setAnnoDescubrimiento(int pAnnoDescubrimiento) {
+        annoDescubrimiento = pAnnoDescubrimiento;
+    }
+    
+    public int getAnnoDescubrimiento() {
+        return annoDescubrimiento;
+    }
+}
+
+
---
+++ TablaPeriodica.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quimica;
+
+import java.io.*; 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+/**
+ *
+ * @author felip
+ */
+public class TablaPeriodica {
+    Elemento[] elementos;
+    
+    public TablaPeriodica() {
+        JSONParser parser = new JSONParser();
+        elementos = new Elemento[118];
+        try {
+            JSONArray arregloElementosJson = (JSONArray) parser.parse(new FileReader("Tabla.json"));
+            for (int i = 0; i < arregloElementosJson.size(); i++) {
+                JSONObject elemento = (JSONObject) arregloElementosJson.get(i);
+                
+                int pesoAtomico = Integer.parseInt(elemento.get("atomicNumber").toString());
+                
+                String simbolo = elemento.get("symbol").toString();
+                
+                String nombre = elemento.get("name").toString();
+                
+                double masaAtomica = Double.parseDouble(elemento.get("atomicMass").toString());
+                
+                String color = elemento.get("cpkHexColor").toString();
+                String configuracionElectronica = elemento.get("electronicConfiguration").toString();
+                
+                String electronegatividadStr = elemento.get("electronegativity").toString();
+                double electronegatividad = (electronegatividadStr.equals("")) ? 0: Double.parseDouble(electronegatividadStr);
+                
+                String radioAtomicoStr = elemento.get("atomicRadius").toString();
+                int radioAtomico = (radioAtomicoStr.equals("")) ? 0: Integer.parseInt(radioAtomicoStr);
+                
+                String radioIonico = elemento.get("ionRadius").toString();
+                
+                String radioVanDelWaalsStr = elemento.get("vanDelWaalsRadius").toString();
+                int radioVanDelWaals = (radioVanDelWaalsStr.equals("")) ? 0: Integer.parseInt(radioVanDelWaalsStr);
+                
+                String energiaIonizacionStr = elemento.get("ionizationEnergy").toString();
+                int energiaIonizacion = (energiaIonizacionStr.equals("")) ? 0: Integer.parseInt(energiaIonizacionStr); 
+                
+                String afinidadElectronicaStr = elemento.get("electronAffinity").toString();
+                int afinidadElectronica = (afinidadElectronicaStr.equals("")) ? 0: Integer.parseInt(afinidadElectronicaStr); 
+                
+                String estadosOxidacionStr = elemento.get("oxidationStates").toString();
+                int[] estadosOxidacion;
+                if (!estadosOxidacionStr.equals("")) {
+                    String[] estadosStr = estadosOxidacionStr.split(",");
+                    estadosOxidacion = new int[estadosStr.length];
+                    int j = 0;
+                    while (j < estadosStr.length) {
+                        estadosOxidacion[j] = Integer.parseInt(estadosStr[j].replace(" ", ""));
+                        j++;
+                    }
+                } else {
+                    estadosOxidacion = new int [0];
+                }
+
+                String estadoEstandar = elemento.get("standardState").toString();
+                
+                String tipo = elemento.get("bondingType").toString();
+                
+                String puntoFusionStr = elemento.get("meltingPoint").toString();
+                int puntoFusion = (puntoFusionStr.equals("")) ? 0: Integer.parseInt(puntoFusionStr); 
+                
+                String puntoEbullicionStr = elemento.get("boilingPoint").toString();
+                int puntoEbullicion = (puntoEbullicionStr.equals("")) ? 0: Integer.parseInt(puntoEbullicionStr); 
+                
+                String densidadStr = elemento.get("density").toString();
+                double densidad = (densidadStr.equals("")) ? 0: Double.parseDouble(densidadStr);
+                
+                String nombreBloque = elemento.get("groupBlock").toString();
+              
+                String annoDescubrimientoStr = elemento.get("yearDiscovered").toString();
+                int annoDescubrimiento = (annoDescubrimientoStr.equals("Ancient")) ? -1: Integer.parseInt(annoDescubrimientoStr); 
+                
+                elementos[i] = new Elemento( nombre, pesoAtomico, simbolo, masaAtomica, color, configuracionElectronica, electronegatividad, radioAtomico, radioIonico, radioVanDelWaals, energiaIonizacion, afinidadElectronica, estadosOxidacion, estadoEstandar, tipo, puntoFusion, puntoEbullicion, densidad, nombreBloque, annoDescubrimiento);
+                
+            }
+        } catch (FileNotFoundException e) {
+            //e.printStackTrace();
+        } catch (IOException e) {
+            //e.printStackTrace();
+        } catch (ParseException e) {
+            //e.printStackTrace(); 
+        } catch (Exception e) { 
+            //e.printStackTrace(); 
+        }
+    }
+    
+    public Elemento[] getElementos() {
+        return elementos;
+    }
+}
